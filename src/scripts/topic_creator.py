079f6e60ecc202d890ca0f9b7ad30aa095b30ffc
Hillary Kipkemoi <tenskap@gmail.com>
2024-06-25 08:34:04 +0300

    get topics from env

--- src/scripts/topic_creator.py
+++ src/scripts/topic_creator.py
@@ -1,4 +1,3 @@
-# scripts/topic_creator.py
 import logging
 import os
 from dotenv import load_dotenv
@@ -7,6 +6,9 @@ from confluent_kafka.admin import AdminClient, NewTopic
 # Load environment variables
 load_dotenv()
 KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9094")
+TOPIC_NAMES = os.getenv("TOPIC_NAMES", "")
+PARTITIONS = int(os.getenv("PARTITIONS", "1"))
+REPLICATION_FACTOR = int(os.getenv("REPLICATION_FACTOR", "1"))
 
 # Set up logging
 logger = logging.getLogger(__name__)
@@ -15,11 +17,11 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 
-def create_topics(topics, bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS):
+def create_topics(topics, num_partitions, replication_factor, bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS):
     """Creates Kafka topics if they don't exist."""
     admin_client = AdminClient({'bootstrap.servers': bootstrap_servers})
 
-    new_topics = [NewTopic(topic, num_partitions=1, replication_factor=1) for topic in topics]
+    new_topics = [NewTopic(topic, num_partitions=num_partitions, replication_factor=replication_factor) for topic in topics]
     fs = admin_client.create_topics(new_topics, operation_timeout=10)  # Add timeout
 
     # Wait for each operation to finish.
@@ -29,11 +31,10 @@ def create_topics(topics, bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS):
             logger.info(f"Topic '{topic}' created (or already exists)")
         except Exception as e:
             logger.error(f"Failed to create topic '{topic}': {e}")
-            # Handle error as needed, maybe exit with a non-zero status code
 
 if __name__ == "__main__":
-    # List the topics you need to create
-    topics = ['stock_data', 'user_registrations', 'backtest_results']  
-
-    create_topics(topics)  # Create the topics
-
+    topics = [topic.strip() for topic in TOPIC_NAMES.split(",")] if TOPIC_NAMES else []
+    if topics:
+        create_topics(topics, PARTITIONS, REPLICATION_FACTOR)
+    else:
+        logger.error("No topics specified in TOPIC_NAMES environment variable.")
